4df28c84426a48c67b1699ac8e6fc3c726e3f86a
Gaute Hope <gauteh@src.gnome.org>
2004-06-22 20:59:41 +0000

    added some cron files

---
+++ src/support.py
@@ -0,0 +1,34 @@
+import gconf
+import os
+gconf_client = gconf.client_get_default()
+
+# gtk.gdk.pixbuf_new_from_file (filename)
+
+
+def nautilus_icon (type):
+	_nautdir = "/usr/share/pixmaps/nautilus"
+	_pixdir = "/usr/share/pixmaps"
+	theme = gconf_client.get_string("/desktop/gnome/file_views/icon_theme")
+	if type == "x-directory/":
+		if theme:
+			newicon = "%s/%s/i-directory.png" % (_nautdir,theme)
+		else:
+			newicon = "%s/gnome-folder.png" % _pixdir
+		if os.path.isfile(newicon):
+			return newicon
+		return None
+	else:
+		icontmp = type.replace('/','-')
+		if theme:
+			newicon = "%s/%s/gnome-%s.png" % (_nautdir,theme,icontmp)
+			if os.path.isfile(newicon):
+				return newicon
+			else:
+				newicon = "%s/%s/%s.png" % (_nautdir,theme,icontmp)
+				if os.path.isfile(newicon):
+					return newicon
+				else:
+					newicon = "%s/document-icons/gnome-%s.png" % (_nautdir,icontmp)
+					if os.path.isfile(newicon):
+						return newicon
+		return None
