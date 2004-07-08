16dba2eb2925ce28879a6ded9d79818e293f3e04
Gaute Hope <gauteh@src.gnome.org>
2004-07-08 15:58:07 +0000

    at now supports preview, had to change a large amount of code since the method used by Philip didn't work. Wen't back to my old way of determinig whats script and whats prepended by at. hope this works.

--- src/at.py
+++ src/at.py
@@ -45,8 +45,21 @@ class At:
 	def __init__(self, parent):
 		self.atRecordRegex = re.compile('([^\s]+)\s([^\s]+)\s([^\s]+)\s([^\s]+)\s([^\s]+)')
 		self.atRecordRegexAdd = re.compile('([^\s]+)\s([^\s]+)\s')
-
 		self.atRecordRegexAdded = re.compile('[^\s]+\s([0-9]+)\sat')
+
+		#getting default prepended stuff by at
+		execute = "echo > /tmp/tmpat && at tomorrow -f /tmp/tmpat && rm /tmp/tmpat"
+		temp = commands.getoutput(execute)
+		startjob = temp.find("job")
+		temp = temp[startjob:]
+		m = self.atRecordRegex.match(temp)
+		job = m.groups ()[0]
+		job_id = m.groups ()[1]
+		execute = "at -c " + job_id
+		at_pre = commands.getoutput(execute)
+		remjob = commands.getoutput("atrm + " + job_id)
+		self.at_pre_len = len(at_pre) - 1
+			
 
 		self.ParentClass = parent
 		self.xml = self.ParentClass.xml
@@ -111,7 +124,8 @@ class At:
 	def delete (self, jobid):
 		if jobid:
 			execute = config.getAtrmbin()+ " " + str(jobid)
-		commands.getoutput(execute)
+			commands.getoutput(execute)
+			print jobid
 		return
 
 	def append (self, runat, command, title, icon):
@@ -142,10 +156,13 @@ class At:
 				else:
 					icon_pix = None
 
-				print lines
+				
 				preview = self.make_preview (lines)
+
 				timestring = _("%s%s%s %s%s%s") % (_(""), date, _(""), _(""), time, _(""))
 				iter = self.ParentClass.treemodel.append([title, timestring, preview, array_or_false, int(job_id), timestring, icon_pix, self, date, class_id, user, "Defined", "at"])
+				
+				#print title + " " + timestring + " " + preview + " " + job_id + " " + date + " " +  class_id + " " + user 
 				# print int(job_id)
 
 		#["None(not suported yet)", "12:50 2004-06-25", "", "35", "", "12:50", icon, at instance, "2004-06-25", "a", "drzap", "at"]
@@ -159,79 +176,45 @@ class At:
 				break
 		return found
 
-	def prepare_script (self, lines):
+	def prepare_script (self, script):
 	
 		# It looks like at prepends a bunch of stuff to each script
 		# Luckily it delimits that using two newlines
 		# So assuming that at never prepends two newlines unless
 		# it's done prepending, we will start recording the custom commands
 		# once the first two lines have been found
-		
-		newlines = []
-		title = None
-		icon = None
-		startrec = gtk.FALSE
-		entarfnd = gtk.FALSE
-		for line in lines:
-		
-			# If we have a line and we have not yet started recording
-			# So all lines in this if-block are prepended by 'at'
-			
-			if startrec == gtk.FALSE and len(line) > 0:
-			
-				# Find the first newline
-				
-				if line[0] == '\n':
-					entarfnd = gtk.TRUE
-					
-				# Find the second newline and else is the first newline
-				# unimportant (so loose the fact that we found it)
-				
-				if entarfnd == gtk.TRUE and line[0] == '\n':
-					startrec = gtk.TRUE
-				else:
-					entarfnd = gtk.FALSE
-			elif startrec == gtk.TRUE:
-				
-				# If we are here, it means the line was not prepended by
-				# 'at' but instead added by the user.
-				
-				if line[len(line)-1] == '\n':
-					# chop last last (which is a newline)
-					line = line[:-1]
-				
-				# If the string contains TITLE=
-				if line.find ("TITLE=") != -1:
-					title = line.split ("=")[1]
-				# If the string contains ICON=
-				elif line.find ("ICON=") != -1:
-					icon =line.split ("=")[1]
-				# Else this is a line of the script
-				else:
-					newlines.append (line)
 
-		return newlines, title, icon
+		# gaute: It now seems like this is incorrect, and may vary upon distribution. I therefore determine the prepended stuff by making a test job and then removing the length of it. at gentoo it adds to newlines at the end of the script
+
+		
+
+
+		# If the string contains TITLE=
+		#if line.find ("TITLE=") != -1:
+		#	title = line.split ("=")[1]
+		# If the string contains ICON=
+		#elif line.find ("ICON=") != -1:
+		#	icon =line.split ("=")[1]
+		# Else this is a line of the script
+		#else:
+		#	newlines.append (line)
+		#	print line
+		title = ""
+		icon = ""
+		script = script[self.at_pre_len:]
+		return script, title, icon
 
 	def make_preview (self, lines):
-		cnt = 0
-		result = ""
-		for line in lines:
-			if len (line) > 0 and line[0] != "#":
-				lcnt = 0
-				for a in line:
-					if a == "\n":
-						a = ";"
-					if cnt <= 15:
-						result = result + a
-					else:
-						break
-					
-					lcnt = lcnt + 1
-				result = result + ";"
-				cnt = cnt + lcnt + 1
-			if cnt > 15:
-				result = result + "..."
-				break
+		result = lines[0:15]
+		result = result.replace("\n",";")
+		done = 0
+		while done == 0:
+			if result[-1] == ";":
+				result = result[0:-1]
+			else:
+				done = 1
+		result = result + "..."
+
 		return result
 
 	def parse (self, line, output = 0):
@@ -247,10 +230,9 @@ class At:
 					user = m.groups ()[4]
 					execute = config.getAtbin() + " -c " + job_id
 					# read lines and detect starter
-					readlines = os.popen(execute).readlines()
-					lines, title, icon = self.prepare_script (readlines)
-
-					return job_id, date, time, class_id, user, lines, title, icon
+					script = os.popen(execute).read()
+					script, title, icon = self.prepare_script (script)
+					return job_id, date, time, class_id, user, script, title, icon
 		else:
 			if len (line) > 1 and line[0] != '#':
 				m = self.atRecordRegexAdd.match(line)
@@ -258,9 +240,10 @@ class At:
 					# print m.groups()
 					job = m.groups ()[0]
 					job_id = m.groups ()[1]
+					print job
+					print job_id
 					return job_id
-		#left unchanged, the fields should be: user, job title, and id
-		#would probably have to do a specific job check for each job 
+
 		return gtk.FALSE
 
 
