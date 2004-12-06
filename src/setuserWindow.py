5f30186f4f76405e9dcbc85ea385ffa6c83b63eb
Kristof Vansant <kvansant@src.gnome.org>
2004-12-06 02:50:41 +0000

    use comboxentry instead

--- src/setuserWindow.py
+++ src/setuserWindow.py
@@ -16,12 +16,13 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-
 #pygtk modules
 import gtk
+import gobject
 
 #python modules
 import pwd
+
 
 ##
 ## I18N
@@ -40,24 +41,37 @@ class SetuserWindow:
 		self.widget = self.xml.get_widget("setuserWindow")
 		self.widget.connect("delete-event", self.on_cancel_button_clicked)
 
-		# TODO: replace with comboBox
 		self.entUser = self.xml.get_widget("entUser")
-
+		
+		liststore = gtk.ListStore(gobject.TYPE_STRING)
+		self.entUser.set_model(liststore)
+		self.entUser.set_text_column(0)
+		
+				
+		#fill combox with all the users
+		pwd_info = pwd.getpwall()
+		
+		for info in pwd_info:
+			self.entUser.append_text(info[0])
+			
 		self.cancel_button = self.xml.get_widget ("setuser_cancel_button")
 		self.ok_button = self.xml.get_widget ("setuser_ok_button")
 		self.xml.signal_connect("on_setuser_cancel_button_clicked", self.on_cancel_button_clicked)
 		self.xml.signal_connect("on_setuser_ok_button_clicked", self.on_ok_button_clicked)
-
+		
+		
 	#public function
 	def ShowSetuserWindow (self):
 		self.widget.show_all()
 
+
 	def on_cancel_button_clicked (self, *args):
 		self.widget.hide()
 
+
 	def on_ok_button_clicked (self, *args):
 		try:
-			user = self.entUser.get_text()
+			user = self.entUser.get_active_text()
 			userdb = pwd.getpwnam(user)
 
 			if user != self.ParentClass.user:
@@ -76,4 +90,4 @@ class SetuserWindow:
 			self.dialog = gtk.MessageDialog(self.widget, gtk.DIALOG_MODAL|gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, "No such user")
 			self.dialog.run ()
 			self.dialog.destroy ()
-			
+		
