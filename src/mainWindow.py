82545eb47eb36d149dde1e451bbbdd6cc0d40a14
Gaute Hope <gauteh@src.gnome.org>
2005-04-07 14:29:45 +0000

    Using euid as source for checking for root permissions on execution

--- src/mainWindow.py
+++ src/mainWindow.py
@@ -284,7 +284,10 @@ class main:
 	
 	
 	def __initUser__(self):
-		self.__setUser__(os.environ['USER'])
+		self.uid = os.geteuid() #The enivronment variable isn't always updated after a 'su'
+		self.gid = os.getegid()
+		self.user = pwd.getpwuid(self.uid)[0]
+		print self.user
 		
 		if self.uid != 0:
 			self.set_user_menu.hide()	
