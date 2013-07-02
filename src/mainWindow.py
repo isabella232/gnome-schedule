5bc371c5df79b048ae859ea7147840c86a435c05
Jiro Matsuzawa <jmatsuzawa@gnome.org>
2013-07-03 00:28:37 +0900

    Make glade UI show translated strings
    https://bugzilla.gnome.org/show_bug.cgi?id=703469

--- src/mainWindow.py
+++ src/mainWindow.py
@@ -17,6 +17,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+# i18n and locale has to be set up before glade in some cases
+import lang
+
 #pygtk modules
 import gtk
 import gtk.glade
